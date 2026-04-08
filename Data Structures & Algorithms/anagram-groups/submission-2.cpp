af869f49464e4953da36e6b196888edd4fc137ea
Sarthak Kashyap <98842340+ROOKIEsarthak@users.noreply.github.com>
2026-04-09 00:45:02 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) { 
+        map <vector<int>,vector<string>> mp;
+        for(int i = 0;i<strs.size();i++){
+            vector<int>arr (26,0); 
+            for(int j = 0;j<strs[i].size();j++){
+                arr[strs[i][j] - 'a']+=1; 
+            }
+            mp[arr].push_back(strs[i]);  
+        }
+        vector<vector<string>> result;
+        for(auto &it : mp){
+            result.push_back(it.second);
+        }
+        return result;
+    }
+};
